8d929a3fa5b6ced7eb68db71036adf0fe0f12a50
ss“git config --global user.email 1915232691@qq.com ssh-keygen -t rsa -C 1915232691@qq.comgit config --global user.name ss“ <1915232691@qq.com>
2021-09-01 22:53:11 +0800

    feat:runInAction的使用

--- src/components/Todos/TodoView.js
+++ src/components/Todos/TodoView.js
@@ -1,16 +1,21 @@
 import { observer } from 'mobx-react-lite';
+import { useTodolistStore } from '../../stores/TodoStore/TodoListStore';
 function TodoView({ todo }) {
+  const todoListStore = useTodolistStore();
   return (
     <li className={todo.completed ? 'completed' : ''}>
       <div className="view">
         <input
-          checkbox={todo.completed}
+          checkbox={todo.completed.toString()}
           onChange={() => todo.toggle()}
           className="toggle"
           type="checkbox"
         />
         <label>{todo.title}</label>
-        <button className="destroy" />
+        <button
+          className="destroy"
+          onClick={() => todoListStore.deleteTdo(todo.title)}
+        />
       </div>
       <input className="edit" />
     </li>
--- src/stores/TodoStore/TodoListStore.js
+++ src/stores/TodoStore/TodoListStore.js
@@ -1,6 +1,13 @@
 import TodoViewStore from './TodoViewStore';
-import { makeObservable, observable, action, computed } from 'mobx';
+import {
+  makeObservable,
+  observable,
+  action,
+  computed,
+  runInAction,
+} from 'mobx';
 import { useContext, createContext } from 'react';
+import axios from 'axios';
 class TodoListStore {
   todos = [];
   constructor(todos) {
@@ -9,13 +16,27 @@ class TodoListStore {
       todos: observable,
       createTodo: action,
       unCompletedTodoCount: computed,
+      deleteTdo: action,
     });
+    this.loadTodos();
   }
   createTodo(title) {
     this.todos.push(new TodoViewStore(title));
   }
+  deleteTdo(title) {
+    this.todos = this.todos.filter((todo) => todo.title !== title);
+  }
   get unCompletedTodoCount() {
     return this.todos.filter((todo) => !todo.completed).length;
+  }
+  //发起异步请求的时候并不能触发action，这个时候需要使用runInAction方法来触发action
+  async loadTodos() {
+    let todos = await axios
+      .get('http://localhost:3005/todo')
+      .then((response) => response.data);
+    runInAction(() =>
+      todos.forEach((todo) => this.todos.push(new TodoViewStore(todo.title)))
+    );
   }
 }
 
